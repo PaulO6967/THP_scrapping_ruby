02f9947931a9881fbade20be356aee00a57c2901
PaulO6967 <p.osswald67@gmail.com>
2023-07-13 16:24:46 +0200

    exo02 plus test done

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -9,19 +9,27 @@ def get_hyperlink_mairie_valdoise
   mairies_link_list =[]
   page_url = "https://www.annuaire-des-mairies.com/val-d-oise.html"
   page = Nokogiri::HTML(URI.open(page_url))
-  page.xpath('/html/body/table/tbody/tr[3]/td/table/tbody/tr/td[2]/p[2]/object/table/tbody/tr[2]/td/table/tbody/tr/td[1]/p/a[1]').each do |city|
-    puts mairies_name_list << city
+  puts "récupération des données en cours"
+  page.css('a[class=lientxt]').each do |city_link|
+    city_link_relative_path = city_link['href']
+    city_link_absolute_path = URI.join(page_url, city_link_relative_path).to_s
+    mairies_link_list << city_link_absolute_path
+    mairies_name_list << city_link.text
   end
-    binding.pry
-    #mairies_name_list << mairie.text
-  #mairies_link_list << mairie.
+  mairies_email_list = []
+  mairies_link_list.each do |townhall_url|
+  mairies_email_list << get_townhall_email(townhall_url)
+  end
+  townhall_email_related_city = []
+  mairies_name_list.each_index do |i|
+    townhall_email_related_city << { mairies_name_list[i] => mairies_email_list[i] }
+    puts townhall_email_related_city
+  end
 end
 
 def get_townhall_email(townhall_url)
   page = Nokogiri::HTML(URI.open(townhall_url))
-  puts page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+  return page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 end
 
 get_hyperlink_mairie_valdoise
-
-#get_townhall_email("http://annuaire-des-mairies.com/95/avernes.html")
--- spec/mairie_christmas_spec.rb
+++ spec/mairie_christmas_spec.rb
@@ -1,8 +1,17 @@
 # test for the file mairie_christmas
-require_relative "../lib/mairie_christmas"
+require_relative "../lib/mairie_christmas.rb"
 
-describe "" do
-  it "" do
-    expect(scraping_array.nil?).to eq(false)
+describe "control of the table" do
+  it "we get an extract" do
+    expect(get_hyperlink_mairie_valdoise.nil?).to eq(false)
+  end
+  it "we get an array" do 
+    expect(get_hyperlink_mairie_valdoise.class).to eq(Array)
+  end
+end
+
+describe "contrôle the scrapped datas" do
+  it "check the correct values for the avernes townhall" do
+    expect(get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html")).to eq("mairie.avernes@orange.fr")
   end
 end
