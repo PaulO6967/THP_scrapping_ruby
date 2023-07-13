ffc92c3521662af3352b2a300a185a6e687a520d
PaulO6967 <p.osswald67@gmail.com>
2023-07-13 12:09:29 +0200

    dark_trader done

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -3,3 +3,25 @@ require 'nokogiri'
 require 'open-uri'
 
 PAGE_URL = "https://coinmarketcap.com/all/views/all/"
+PAGE = Nokogiri::HTML(URI.open(PAGE_URL))
+
+def scraping_array
+  page = Nokogiri::HTML(URI.open(PAGE_URL))
+  puts "Recuparation des données en cours :"
+  symbols_array = []
+  page.xpath('//tr/td[3]/div').each do |symbol|
+    symbols_array << symbol.content
+  end
+  prices_array = []
+  page.xpath('//tr/td[5]/div').each do |symbol|
+    prices_array << symbol.content
+  end
+  crypto_symbols_by_prices_arrey = []
+  (0..(symbols_array.size - 1)).each do |crypto|
+    crypto_symbols_by_prices_arrey << { symbols_array[crypto] => prices_array[crypto] }
+  end
+  puts crypto_symbols_by_prices_arrey
+  return crypto_symbols_by_prices_arrey
+end
+
+scraping_array
--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,2 +1,14 @@
+
 require_relative "../lib/dark_trader.rb"
 
+describe "the programme is working by returning an array of hash" do
+  it "test if the return is good" do
+    expect(scraping_array.nil?).to eq(false)
+  end
+end
+
+describe "contrôle the scrapped datas" do
+  it "check the cryptocurrencies presence" do
+    expect(scraping_array.size).to eq(20)
+  end
+end
